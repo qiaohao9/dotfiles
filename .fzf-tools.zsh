1dfcf10adacffbd4cead7e0854dde98103114796
tinyRat <me@tinyrat.me>
2019-03-22 09:57:06 +0800

    zsh: reformat fzf-tools

---
+++ .fzf-tools.zsh
@@ -0,0 +1,88 @@
+# ================
+# Brew Management
+# ================
+function install() {
+	local inst=$(brew search | fzf -m)
+	if [[ $inst ]]; then
+		for prog in $(echo $inst); do
+			brew install $prog
+		done
+	fi
+}
+
+function rmtree() {
+	local uninstall=$(brew leaves | fzf -m)
+
+	if [[ $uninstall ]]; then
+		for prog in $(echo $uninstall);do
+			brew rmtree $prog
+		done
+	fi
+}
+
+function zap() {
+	local token
+	token=$(brew cask list | fzf-tmux --query="$1" +m --preview "brew cask info {}")
+
+	if [[ x"$token" != "x" ]]; then
+		brew cask zap $token
+	fi
+}
+
+#=================
+# Command Support
+#=================
+function fh() {
+	eval $(cat ~/.history | fzf +s +x --tac --height 20% | sed 's/ *[0-9]* *//')
+}
+
+function fssh() {
+	ssh_name=$(awk '/Host / { for(i=2;i<=NF;i++) printf "%s\n", $i }' $HOME/.ssh/config | fzf -m --height 20%)
+	
+	if [[ $ssh_name ]]; then
+		ssh -C $ssh_name
+	fi
+}
+
+# gitignore.io
+function __git() {
+	curl -L -s https://gitignore.io/api/"$@"
+}
+function gi() {
+	if [ "$#" -eq 0 ]; then
+		IFS+=","
+		for item in $(__git list); do
+			echo $item
+		done | fzf --height 20% --multi --ansi | paste -s -d "," - |
+		{ read result && __git "$result" ; } > ./.gitignore
+	else
+		__git "$@" > ./.gitignore
+	fi
+}
+
+# tmux session
+function tm() {
+	[[ -n "$TMUX" ]] &&  change="switch-client" || change="attach-session"
+	
+	if [[ $1 ]]; then
+		tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+	fi
+
+	session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) && tmux $change -t "$session" || echo "No session found."
+}
+
+function proxy() {
+	if [[ ! ${http_proxy} ]]; then
+		export http_proxy=http://127.0.0.1:1087
+		export https_proxy=http://127.0.0.1:1087
+	fi
+}
+
+# share files into file.io
+function share() {
+    if [ $# != 0 ]; then
+        curl -F "file=@$1" "https://file.io/?expires=1h"
+    else
+        echo "No specific fiels."
+    fi
+}
