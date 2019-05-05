797edb1d5fde5603928de627671b7273c737107b
vastpeng <keatingsmitht@gmail.com>
2019-05-05 14:35:45 +0800

    zsh: remove ip

--- .fzf-tools.zsh
+++ .fzf-tools.zsh
@@ -78,13 +78,3 @@ function proxy() {
 	fi
 }
 
-
-# Get my ip
-function ip() {
-    if [[ $1 -eq 6 ]]; then
-        curl -6 -s ifconfig.co/json | fx
-    else
-        curl -4 -s ifconfig.co/json | fx
-    fi
-
-}
