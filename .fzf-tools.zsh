3088cc420c71e629ee3ef9895df1b39b774818da
vastpeng <keatingsmitht@gmail.com>
2019-05-13 11:23:32 +0800

    zsh: add fkill

--- .fzf-tools.zsh
+++ .fzf-tools.zsh
@@ -78,3 +78,18 @@ function proxy() {
 	fi
 }
 
+# Kill process
+# fkill - kill processes - list only the ones you can kill. Modified the earlier script.
+fkill() {
+    local pid
+    if [ "$UID" != "0" ]; then
+        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+    else
+        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    fi
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+    fi
+}
