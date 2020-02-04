f375626a2569ff8d944393425c87d891134b3874
tinyRat <me@tinyrat.me>
2020-02-04 13:12:42 +0800

    add pycharm to english-input-method

--- .hammerspoon/config.lua
+++ .hammerspoon/config.lua
@@ -2,12 +2,7 @@
 appInputMethod = {
     {'/Applications/Xcode.app', 'English'},
     {'/Applications/Visual Studio Code.app', 'English'},
+    {'/Applications/PyCharm.app', 'English'},
     {'/Applications/Hyper.app', 'English'},
 }
 
--- Clipboard --
-frequency = 1.0 -- Speed in seconds to check for clipboard changes. If you check too frequently, you will loose performance, if you check sparsely you will loose copies
-hist_size = 10  -- How many items to keep on history
-label_length = 70 -- How wide (in characters) the dropdown menu should be. Copies larger than this will have their label truncated and end with "…" (unicode for elipsis ...)
-honor_clearcontent = false --asmagill request. If any application clears the pasteboard, we also remove it from the history https://groups.google.com/d/msg/hammerspoon/skEeypZHOmM/Tg8QnEj_N68J
-pasteOnSelect = false -- Auto-type on click
