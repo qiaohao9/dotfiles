9573ec3aeb840de9ab26855aa3829a8493a27a4c
tinyRat <me@tinyrat.me>
2020-02-24 19:07:22 +0800

    theme: dracula

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["nord-hyper", "hypercwd", "hyper-fading-scrollbar"],
+  plugins: ["hypercwd", "hyper-fading-scrollbar", "hyper-dracula"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
