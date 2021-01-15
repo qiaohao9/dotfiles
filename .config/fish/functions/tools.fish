0c6f0a15de5528a26b4acc18b906df26a67d8b99
qiaohao <qiaohao.peng@outlook.com>
2021-01-15 17:40:01 +0800

    add(fish): function brewfile()

--- .config/fish/functions/tools.fish
+++ .config/fish/functions/tools.fish
@@ -7,6 +7,11 @@ function proxy -d "You know what the GFW is."
     set -gx https_proxy http://127.0.0.1:7890
 end
 
-function geoip -d "Update the Country.mmdb"
+function geoip -d "Update the Country.mmdb."
     curl -o $HOME/.config/clash/Country.mmdb -L "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb"
 end
+
+function brewfile -d "Backup applications installed by HomeBrew."
+    brew bundle dump --global -f
+end
+
