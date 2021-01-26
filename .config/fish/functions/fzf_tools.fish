575e8fc631fdc2c2c0c2a26e6fc8db40d0d05004
qiaohao <qiaohao.peng@outlook.com>
2021-01-26 10:42:27 +0800

    fix(fish): ssh host regepx

--- .config/fish/functions/fzf_tools.fish
+++ .config/fish/functions/fzf_tools.fish
@@ -1,5 +1,5 @@
 function fssh -d "Fuzzy-find ssh host via ag and ssh into it."
-    awk '/^Host [a-zA-Z0-9-]+$/ { for(i=2; i<=NF; i++) printf "%s\n", $i }' $HOME/.ssh/config | fzf +m --height 20% | read -l ssh_target; and ssh "$ssh_target"
+    awk '/^Host [a-zA-Z0-9\-_]+$/ { for(i=2; i<=NF; i++) printf "%s\n", $i }' $HOME/.ssh/config | fzf +m --height 20% | read -l ssh_target; and ssh "$ssh_target"
 end
 
 function ftmux -d "Fuzzy-switch tmux session."
